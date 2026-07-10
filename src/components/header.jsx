d219e296bc43d5a27448a84d22f312c1e06e6623
redlinefree-lab <redlinefree@gmail.com>
2026-07-10 14:43:33 +0700

    revisi header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,16 +4,16 @@ import { Container, Row, Col } from 'react-bootstrap';
 // FUNGSI KOMPONEN HEADER 
 function Header() {
     return (
-        <>
+        <div className ='hero min-vh-92 w-100 '>                      
             <Container>
                 <Row>
-                    <Col>
-                        <h1>Mary BerSantai Sejenak</h1>
-                        <p>Selamat datang di website kami! Kami senang Anda berada di sini. Jelajahi berbagai layanan dan produk yang kami tawarkan, dan jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan atau membutuhkan bantuan. Terima kasih telah mengunjungi kami!</p>
-                    </Col>
+                <Col>
+                    <h1>Mary BerSantai Sejenak</h1>
+                    <p>Selamat datang di website kami! Kami senang Anda berada di sini. Jelajahi berbagai layanan dan produk yang kami tawarkan, dan jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan atau membutuhkan bantuan. Terima kasih telah mengunjungi kami!</p>
+                </Col>
                 </Row>
             </Container>
-        </>
+        </div>
     );
 }
 
